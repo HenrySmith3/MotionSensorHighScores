b3ded0d53e6d6f9d58409983ff1d1bccf359b483
HenrySmith3 <henry.smith@gatech.edu>
2013-05-26 15:16:48 -0400

    Added the Z component

--- src/com/example/MotionSensorHighScores/Accelerometer.java
+++ src/com/example/MotionSensorHighScores/Accelerometer.java
@@ -19,9 +19,13 @@ public class Accelerometer extends Activity implements SensorEventListener {
     boolean started = false;
     SensorManager mSensorManager;
     Sensor mAccelerometer;
-    int maxAccel = 0;
+    float maxAccelX = 0;
+    float maxAccelY = 0;
+    float maxAccelZ = 0;
     Button button;
-    TextView text;
+    TextView textX;
+    TextView textY;
+    TextView textZ;
     /**
      * Called when the activity is first created.
      */
@@ -31,7 +35,9 @@ public class Accelerometer extends Activity implements SensorEventListener {
         setContentView(R.layout.accelerometer);
 
         button = (Button) findViewById(R.id.startButton);
-        text = (TextView) findViewById(R.id.textView);
+        textX = (TextView) findViewById(R.id.textViewX);
+        textY = (TextView) findViewById(R.id.textViewY);
+        textZ = (TextView) findViewById(R.id.textViewZ);
 
         mSensorManager =  (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
@@ -45,6 +51,24 @@ public class Accelerometer extends Activity implements SensorEventListener {
         @Override
         public void onClick(View view) {
             started = true;
+            button.setText("Stop");
+            button.setOnClickListener(stopListener);
+            maxAccelX = 0;
+            maxAccelY = 0;
+            maxAccelZ = 0;
+            textX.setText("X: 0");
+            textY.setText("Y: 0");
+            textZ.setText("Z: 0");
+        }
+    };
+
+    public View.OnClickListener stopListener = new View.OnClickListener() {
+
+        @Override
+        public void onClick(View view) {
+            started = false;
+            button.setText("Start");
+            button.setOnClickListener(startListener);
         }
     };
 
@@ -60,29 +84,25 @@ public class Accelerometer extends Activity implements SensorEventListener {
 
     @Override
     public void onSensorChanged(SensorEvent event) {
-        if (event.sensor.getType() != Sensor.TYPE_ACCELEROMETER)
+        if (event.sensor.getType() != Sensor.TYPE_ACCELEROMETER || started == false)
             return;
-        float mSensorX = 0, mSensorY = 0;
+        float mSensorX = 0, mSensorY = 0, mSensorZ = 0;
         Display display = ((WindowManager) getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
-        switch (display.getRotation()) {
-            case Surface.ROTATION_0:
-                mSensorX = event.values[0];
-                mSensorY = event.values[1];
-                break;
-            case Surface.ROTATION_90:
-                mSensorX = -event.values[1];
-                mSensorY = event.values[0];
-                break;
-            case Surface.ROTATION_180:
-                mSensorX = -event.values[0];
-                mSensorY = -event.values[1];
-                break;
-            case Surface.ROTATION_270:
-                mSensorX = event.values[1];
-                mSensorY = -event.values[0];
+        mSensorX = event.values[0];
+        mSensorY = event.values[1];
+        mSensorZ = event.values[2];
+        if (Math.abs(mSensorX) > maxAccelX) {
+            textX.setText("X: " + Math.abs(mSensorX));
+            maxAccelX = Math.abs(mSensorX);
         }
-        text.setText("X: " + mSensorX + ", Y: " + mSensorY);
-
+        if (Math.abs(mSensorY) > maxAccelY) {
+            textY.setText("Y: " + Math.abs(mSensorY));
+            maxAccelY = Math.abs(mSensorY);
+        }
+        if (Math.abs(mSensorZ) > maxAccelZ) {
+            textZ.setText("Z: " + Math.abs(mSensorZ));
+            maxAccelZ = Math.abs(mSensorZ);
+        }
     }
 
     @Override
